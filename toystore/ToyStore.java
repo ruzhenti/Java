f27826864cd6ff087ba9da48849fc67ed7069d2b
ruzhenti <99267013+ruzhenti@users.noreply.github.com>
2023-03-20 19:56:16 +0300

    Add files via upload

---
+++ toystore/ToyStore.java
@@ -0,0 +1,36 @@
+package toystore;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class ToyStore {
+    private List<Toy> toys;
+
+    public ToyStore(List<Toy> toys) {
+        this.toys = toys;
+    }
+    
+    public Toy getToyForPrice() {
+        RandomToyChooser random = new RandomToyChooser();
+        Toy toy = random.chooseOnWeight(toys);
+        return toy;
+    }
+
+    public void saveToyForLottery() {
+        Toy toy = getToyForPrice();
+        String text = toy.toString();
+        System.out.println(text);
+        try(FileWriter writer = new FileWriter("C:/Users/User/toystore/Toys.txt", true))
+        { 
+            writer.write(text);
+            writer.append('\n');
+            writer.flush();
+        }
+        catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+        toys.remove(toy);
+    }
+}
